d22872ac977fddcad3aea0e619325e10de9b838c
Hernan Razo <hrazo@hawk.iit.edu>
2019-12-12 22:05:30 -0600

    added comments to code

--- script.py
+++ script.py
@@ -6,12 +6,15 @@ from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 
-stemmer = SnowballStemmer('english')
+#set stopwords
 stop_words = nltk.corpus.stopwords.words('english')
 custom = ['?','(', ')', '.', '[', ']','!', '...',
         ';', "`", "'", '"',',', ':', '*', '~' , '/', '//', '\\']
 stop_words.extend(custom)
 
+#clean dataset by removing twitter mentions, links, and emojis
+#set all letters to lowercase
+#remove stopwords, tokenize, and lemmatize tweets
 def clean_data(raw_data):
 
     remove_mentions = re.sub(r'@[A-Za-z0-9]+', '', raw_data)
@@ -28,35 +31,39 @@ def clean_data(raw_data):
 
 def main():
 
+    #initialize variables and lists
     count = 0
     tweet_list =[]
     list_list = []
+
+    #access csv file and read out data
     csv_file = open('tweet_data.csv')
     data = csv.reader(csv_file)
 
+    #gather all tweets and apply clean)data() function
     for row in data:
-
         string_row = str(row).strip('[]')
         tweets = clean_data(string_row)
 
         for word in tweets:
             tweet_list.append(word)
 
+    #place into one giant list
     list_list.append(tweet_list)
+
+    #place all text into a bag of words model
     dictionary = gensim.corpora.Dictionary(list_list)
+
+    #run dictionary into a corpus to get frequency
     bow_corpus = [dictionary.doc2bow(i) for i in list_list]
 
 
-#actual training???
+    #train model
     lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=4, id2word=dictionary, passes=10, workers=2)
 
     for idx, topic in lda_model.print_topics(-1):
         print("Topic: {} \nWords: {}".format(idx, topic))
         print('\n')
 
-
-
-
 if __name__ == "__main__":
-
     main()
